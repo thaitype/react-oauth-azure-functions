2ee6ea75211ebd4c2fb942954cf2f86fe07567b3
Thada Wangthammang <thada.wth@gmail.com>
2023-10-06 21:19:46 +0700

    refactor az functions

--- apps/api/src/functions/hello.ts
+++ apps/api/src/functions/hello.ts
@@ -1,17 +1,21 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
 
-export async function hello(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+app.http("hello", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (
+    request: HttpRequest,
+    context: InvocationContext
+  ): Promise<HttpResponseInit> => {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get("name") || (await request.text()) || "world";
 
     return { body: `Hello, ${name}!` };
-};
-
-app.http('hello', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: hello
+  },
 });
- 
-
